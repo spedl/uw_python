146588127b91c79b5809796a5da43e75a005c917
Jacob Speidel <jacob.speidel@gmail.com>
2012-01-14 20:03:39 -0800

    The server now returns with my username

--- echo/echo_client.py
+++ echo/echo_client.py
@@ -7,12 +7,12 @@ Both host and port are optional, defaults: localhost 50000
 host must be present if you want to provide port
 """
 
-import socket 
+import socket
 import sys
 
-host = 'localhost' 
-port = 50000 
-size = 1024 
+host = 'localhost'
+port = 50000
+size = 1024
 
 nargs = len(sys.argv)
 if nargs > 1:
@@ -21,10 +21,10 @@ if nargs > 2:
     port = int(sys.argv[2])
 
 s = socket.socket(socket.AF_INET, 
-                  socket.SOCK_STREAM) 
-s.connect((host,port)) 
-s.send('Hello, world') 
-data = s.recv(size) 
-s.close() 
+                  socket.SOCK_STREAM)
+s.connect((host,port))
+s.send('Hello, world')
+data = s.recv(size)
+s.close()
 print 'from (%s,%s) %s' % (host, port, data)
 
